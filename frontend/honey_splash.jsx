5b3d4171f8adc0dfa3fa914e068f7bde2084cf98
Susanna <royalpolaris@gmail.com>
2018-11-14 10:22:39 -0500

    Add greeting and session form components

--- frontend/honey_splash.jsx
+++ frontend/honey_splash.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {login, logout, signup} from './util/session_api_util'
+import configureStore from './store/store'
+import Root from './components/root';
+// import {login, logout, signup} from './util/session_api_util'
+import {login, logout, signup} from './actions/session_actions'
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
   window.signup = signup;
-
-  // login({username: "u1", password: "111111"});
-
-  ReactDOM.render(<h1>Welcome to HoneySplash</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 })
