75d613be27cd8b890e80651959e972c5d6c604ce
Susanna <royalpolaris@gmail.com>
2018-11-16 13:56:04 -0500

    Add list controller

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,2 +1,16 @@
 class Api::ListsController < ApplicationController
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+    else
+      render json: @list.errors.full_messages #status...
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
