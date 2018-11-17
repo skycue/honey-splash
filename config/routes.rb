0845eb83040ebeaf8fc6854c99b32654e90fc14f
Susanna <royalpolaris@gmail.com>
2018-11-17 14:54:12 -0500

    Add routes for lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
+    resources :users, only: [:create] do
+      resources :lists, only: [:create, :destroy, :index, :update]
+    end
     resource :session, only: [:create, :destroy]
   end
 end
