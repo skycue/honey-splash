4d3eb915e7081d5552c3fcb1ffe12732c93a9d27
Susanna <royalpolaris@gmail.com>
2018-11-17 14:52:50 -0500

    Add controller for lists

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,10 +1,34 @@
 class Api::ListsController < ApplicationController
+
+  # before_action :require_logged_in
+
   def create
     @list = List.new(list_params)
+    @list.user_id = params[:user_id]
 
     if @list.save
+      render 'api/lists/show'
     else
       render json: @list.errors.full_messages #status...
+    end
+  end
+
+  # def index
+  #
+  # end
+  #
+  # def update
+  #
+  # end
+
+  def destroy
+    @list = List.find_by_user_id(current_user.id)
+
+    if @list
+      @list.destroy
+      render "api/lists/show"
+    else
+      render json: ["This list does not exist"] #status
     end
   end
 
