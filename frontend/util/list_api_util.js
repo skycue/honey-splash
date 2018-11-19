70658c2c80badb35a0fd18095e1b14b3072dd6ac
Susanna <royalpolaris@gmail.com>
2018-11-19 09:43:14 -0500

    Add api util for list

---
+++ frontend/util/list_api_util.js
@@ -0,0 +1,28 @@
+export const fetchLists = user_id => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/users/${user_id}/lists`,
+  })
+);
+
+// export const fetchList = (user_id, list_id) => (
+//   $.ajax({
+//     method: 'GET',
+//     url: `/api/users/${user_id}/lists/${list_id}`
+//   })
+// );
+
+export const createList = (user_id, list) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/users/${user_id}/lists`,
+    data: { list }
+  })
+);
+
+export const removeList = (user_id, list_id) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/users/${user_id}/lists/${list_id}`
+  })
+);
