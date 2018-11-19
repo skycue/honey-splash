c42a497de5e488ed7cfe0231fe6364b03b561757
Susanna <royalpolaris@gmail.com>
2018-11-19 10:07:47 -0500

    Add lists reducer

---
+++ frontend/reducers/lists_reducer.js
@@ -0,0 +1,24 @@
+import merge from 'lodash/merge';
+
+import { RECEIVE_LISTS, RECEIVE_LIST, REMOVE_LIST } from '../actions/list_actions';
+
+const listsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_LISTS:
+      const lists= {};
+      action.lists.forEach(list => {
+        lists[list.id] = list;
+      });
+      return lists;
+    case RECEIVE_LIST:
+      return merge({}, state, { [action.list.id]: action.list })
+    case REMOVE_LIST:
+      let newState = merge({}, state)
+      delete newState[action.list_id]
+    default:
+      return state;
+  }
+}
+
+export default listsReducer;
