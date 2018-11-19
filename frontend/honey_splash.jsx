32927121142c265699a03c21547d668ae93febb9
Susanna <royalpolaris@gmail.com>
2018-11-19 10:15:21 -0500

    Import and add methods temporarily for testing purposes

--- frontend/honey_splash.jsx
+++ frontend/honey_splash.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store'
 import Root from './components/root';
 // import {login, logout, signup} from './util/session_api_util'
-import {login, logout, signup} from './actions/session_actions'
+import { login, logout, signup } from './actions/session_actions';
+import { createList, removeList } from './util/list_api_util'; //For testing
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
       session: { id: window.currentUser.id }
     };
     store = configureStore(preloadedState);
+    window.store = store; //For testing
     delete window.currentUser;
   } else {
     store = configureStore();
@@ -29,5 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+
+  window.createList = createList;
+  window.removeList = removeList;
+
   ReactDOM.render(<Root store={store}/>, root);
 })
