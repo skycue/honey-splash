d51803b1666628031362cc689658cb363fe7f5fd
Susanna <royalpolaris@gmail.com>
2018-11-20 16:37:20 -0500

    Add list show component

---
+++ frontend/components/list_show/list_show.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ListShow extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="list-show">
+      </div>
+    )
+  }
+}
+
+export default ListShow;
