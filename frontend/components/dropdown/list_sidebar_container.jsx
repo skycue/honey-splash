348f9715ab00a4f9f106ad631877720b8bcd0f2d
Susanna <royalpolaris@gmail.com>
2018-11-21 09:31:45 -0500

    Add setCurrentList function to props

--- frontend/components/dropdown/list_sidebar_container.jsx
+++ frontend/components/dropdown/list_sidebar_container.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { createList, fetchLists } from '../../actions/list_actions';
 import ListSidebar from './list_sidebar';
-import { deleteList } from '../../actions/list_actions';
+import { deleteList, setCurrentList } from '../../actions/list_actions';
 
 const mapStateToProps = state => {
   return {
@@ -14,7 +14,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     fetchLists: (user_id) => dispatch(fetchLists(user_id)),
-    deleteList: (user_id, list_id) => dispatch(deleteList(user_id, list_id))
+    deleteList: (user_id, list_id) => dispatch(deleteList(user_id, list_id)),
+    setCurrentList: (list_id) => dispatch(setCurrentList(list_id))
   };
 };
 
