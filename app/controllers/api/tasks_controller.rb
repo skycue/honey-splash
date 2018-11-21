72720ba864ed2c690959c9bc4401236eca98474b
Susanna <royalpolaris@gmail.com>
2018-11-21 10:24:33 -0500

    Update tasks controller

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,41 @@
 class Api::TasksController < ApplicationController
+
+  def create
+    @task = Task.new(task_params)
+    @task.list_id = params[:list_id]
+    if @task.save
+      render 'api/tasks/show'
+    else
+      render json: @task.errors.full_messages
+  end
+
+  def index
+    @tasks = List.find(params[:list_id]).tasks
+  end
+
+  def update
+    @task = List.find(params[:list_id]).tasks.find(params[:id])
+    if @task.update_attributes(task_params)
+      render 'api/tasks/show'
+    else
+      render json: @task.errors.full_messages
+    end
+  end
+
+  def destroy
+    @task = List.find(params[:list_id]).tasks.find(params[:id])
+
+    if @task
+      @task.destroy
+      render 'api/tasks/show'
+    else
+      render json: ["This task does not exist"]
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
