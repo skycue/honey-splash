058bc20bd73441bd8a5c4e5270ead5cbf5ac5c3d
Susanna <royalpolaris@gmail.com>
2018-11-21 09:34:12 -0500

    Add currentListId from ui slice of state to props

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import Greeting from './greeting';
 
-const mapStateToProps = ({ session, entities: { users } }) => {
+const mapStateToProps = ({ session, entities: { users }, ui }) => {
   return {
-    currentUser: users[session.id]
+    currentUser: users[session.id],
+    currentListId: ui.currentListId
   };
 };
 
