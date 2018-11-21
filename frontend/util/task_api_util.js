f75efbd416d8215e2cd3436964e141530ccc760d
Susanna <royalpolaris@gmail.com>
2018-11-21 17:21:40 -0500

    Fix syntax

--- frontend/util/task_api_util.js
+++ frontend/util/task_api_util.js
@@ -5,22 +5,15 @@ export const fetchTasks = list_id => (
   })
 );
 
-// export const fetchList = (user_id, list_id) => (
-//   $.ajax({
-//     method: 'GET',
-//     url: `/api/users/${user_id}/lists/${list_id}`
-//   })
-// );
-
-export const createTask = (list_id, task) => (
-  $.ajax({
+export const createTask = (list_id, task) => {
+  return $.ajax({
     method: 'POST',
     url: `/api/lists/${list_id}/tasks`,
     data: { task }
   })
-);
+}
 
-export const updateList = (list_id, task) => (
+export const updateTask = (list_id, task) => (
   $.ajax({
     method: 'PATCH',
     url: `/api/lists/${list_id}/tasks/${task.id}`,
@@ -28,7 +21,7 @@ export const updateList = (list_id, task) => (
   })
 );
 
-export const removeList = (list_id, task_id) => (
+export const removeTask = (list_id, task_id) => (
   $.ajax({
     method: 'DELETE',
     url: `/api/lists/${list_id}/tasks/${task_id}`
