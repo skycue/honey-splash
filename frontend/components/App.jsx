0a680b09725f6299901b606d1265fbb1c60810a6
Susanna <royalpolaris@gmail.com>
2018-11-21 17:31:15 -0500

    Fix order of routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-// import { Route } from 'react-router-dom'; //May not need anymore because of Authroute
-// import GreetingContainer from './greeting/greeting_container';
-// import LoginFormContainer from './session_form/login_form_container';
-// import SignupFormContainer from './session_form/signup_form_container';
-// import { AuthRoute } from '../util/route_util';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Session from './session/session';
 import GreetingContainer from './greeting/greeting_container';
 import Modal from './modal/modal';
@@ -12,9 +7,11 @@ import Modal from './modal/modal';
 const App = () => (
   <div className="app">
     <Modal/>
-    <Route exact path="/" component={GreetingContainer}/>
-    <Route exact path="/login" component={Session}/>
-    <Route exact path="/signup" component={Session}/>
+    <Switch>
+      <Route exact path="/login" component={Session}/>
+      <Route exact path="/signup" component={Session}/>
+      <Route path="/" component={GreetingContainer}/>
+    </Switch>
   </div>
 );
 
