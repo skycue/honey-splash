199693bece8eaeed7a8c7a16998ffa5a743db955
Susanna <royalpolaris@gmail.com>
2018-11-21 09:28:11 -0500

    Add case for action SET_CURRENT_LIST

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,9 +1,12 @@
 import { OPEN_MODAL } from '../actions/modal_actions';
+import { SET_CURRENT_LIST } from '../actions/list_actions';
 
 export default function uiReducer(state = {}, action) {
   switch (action.type) {
     case OPEN_MODAL:
       return Object.assign({}, state, action.options);
+    case SET_CURRENT_LIST:
+      return Object.assign({}, state, {currentListId: action.list_id})
     default:
       return state;
   }
