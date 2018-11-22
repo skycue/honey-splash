22842f17f8a68512a1f4f01d518dcab1c0e99ea9
Susanna <royalpolaris@gmail.com>
2018-11-22 17:26:49 -0500

    Add cases for REMOVE_TASK and RECEIVE_TASK actions

--- frontend/reducers/lists_reducer.js
+++ frontend/reducers/lists_reducer.js
@@ -1,5 +1,5 @@
 import merge from 'lodash/merge';
-import { RECEIVE_TASK } from '../actions/task_actions';
+import { RECEIVE_TASK, REMOVE_TASK } from '../actions/task_actions';
 
 import { RECEIVE_LISTS, RECEIVE_LIST, REMOVE_LIST } from '../actions/list_actions';
 
@@ -13,16 +13,21 @@ const listsReducer = (state = {}, action) => {
       //   lists[list.id] = list;
       // });
       // return lists;
-      return merge({}, state, action.lists)
+      return merge({}, state, action.lists);
     case RECEIVE_LIST:
-      return merge({}, state, { [action.list.id]: action.list })
+      return merge({}, state, { [action.list.id]: action.list });
     case REMOVE_LIST:
-      newState = merge({}, state)
-      delete newState[action.list_id]
+      newState = merge({}, state);
+      delete newState[action.list_id];
       return newState;
     case RECEIVE_TASK:
-      newState = merge({}, state)
-      newState[action.task.list_id].task_ids.push(action.task.id)
+      newState = merge({}, state);
+      newState[action.task.list_id].task_ids.push(action.task.id);
+      return newState;
+    case REMOVE_TASK:
+      newState = merge({}, state);
+      let listTaskIds = newState[action.task.list_id].task_ids;
+      listTaskIds.splice(listTaskIds.indexOf(action.task.id), 1);
       return newState;
     default:
       return state;
