72b0ccd2ed1a0887a8944849ad04549d8f0bf5e4
Susanna <royalpolaris@gmail.com>
2018-11-21 22:09:50 -0500

    Update props passed to list show component

--- frontend/components/list_show/list_show_container.jsx
+++ frontend/components/list_show/list_show_container.jsx
@@ -1,13 +1,35 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import ListShow from './list_show';
+import { createTask, fetchTasks } from '../../actions/task_actions';
 
-const mapStateToProps = ({entities: { lists }, ui: {currentListId}}) => {
+const mapStateToProps = ({entities: { tasks }, entities: { lists }, ui: {currentListId}}) => {
+  const currentList = lists[currentListId];
+  const currentTasks = [];
+  if (currentList) {
+    currentList.task_ids.forEach(id => {
+      if (tasks[id]) {
+        debugger
+        currentTasks.push(tasks[id]);
+      }
+    })
+  }
   return {
-    currentList: lists[currentListId]
+    currentList,
+    tasks: currentTasks
   };
 };
 
-export default connect(
+const mapDispatchToProps = dispatch => {
+  return {
+    createTask: (list_id, task) => {
+      return dispatch(createTask(list_id, task))
+    },
+    fetchTasks: (list_id) => dispatch(fetchTasks(list_id))
+  };
+};
+
+export default withRouter(connect(
   mapStateToProps,
-  null
-)(ListShow);
+  mapDispatchToProps
+)(ListShow));
