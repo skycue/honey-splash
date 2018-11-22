68e3b7b51e4729e95a2e65da73dc50fc77fb7a73
Susanna <royalpolaris@gmail.com>
2018-11-22 17:46:45 -0500

    Add onClick handlers

--- frontend/components/task_item/task_item.jsx
+++ frontend/components/task_item/task_item.jsx
@@ -3,12 +3,70 @@ import React from 'react';
 class TaskItem extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      selected: false,
+      openEditForm: false
+    }
+
+    this.toggleSelectTask = this.toggleSelectTask.bind(this);
+    this.toggleSelectAndEditTask = this.toggleSelectAndEditTask.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    debugger
+  }
+
+  toggleSelectTask(e, selectedTask) {
+    e.preventDefault();
+
+    if (this.state.selected) {
+      this.setState({
+        selected: false
+      })
+      this.props.deselectTask(selectedTask);
+    } else {
+      this.setState({
+        selected: true
+      })
+      this.props.selectTask(selectedTask);
+    }
+  }
+
+  toggleSelectAndEditTask(e, selectedTask) {
+    debugger
+    if (this.props.match.params.task_id) {
+      this.setState({
+        selected: false,
+        openEditForm: false
+      })
+    }
+    this.toggleSelectTask(e, selectedTask);
+    if (this.state.openEditForm) {
+      this.setState({
+        openEditForm: false
+      })
+      this.props.history.push(`/lists/${this.props.currentListId}`);
+    } else {
+      this.setState({
+        openEditForm: true
+      })
+       // #1 Check this.props.match.params.task_id && path
+      // if (this.props.match.params.task_id) {
+      //   this.props.deselectTask(this.props.tasks[this.props.match.params.task_id]);
+      //
+      // }
+
+      this.props.history.push(`/lists/${this.props.currentListId}/tasks/${this.props.task.id}`);
+    }
   }
 
   render() {
     return (
       <li>
-        {this.props.task.id}
+        <i onClick={(e) => this.toggleSelectTask(e, this.props.task)} class="material-icons">check_box_outline_blank</i>
+        <i class="material-icons">check</i>
+        <h3 onClick={(e) => this.toggleSelectAndEditTask(e, this.props.task)}>{this.props.task.id}</h3>
       </li>
     );
   }
--- frontend/components/task_item/task_item_container.jsx
+++ frontend/components/task_item/task_item_container.jsx
@@ -1,11 +1,22 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import TaskItem from './task_item';
+import { selectTask, deselectTask } from '../../actions/task_actions';
 
 const mapStateToProps = (state, ownProps) => {
-  debugger
+  
   return {
-    task: ownProps.task
+    task: ownProps.task,
+    tasks: state.entities.tasks,
+    currentListId: state.ui.currentListId
   }
 };
 
-export default connect(mapStateToProps, null)(TaskItem);
+const mapDispatchToProps = dispatch => {
+  return {
+    selectTask: (selectedTask) => dispatch(selectTask(selectedTask)),
+    deselectTask: (selectedTask) => dispatch(deselectTask(selectedTask))
+  }
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TaskItem));
