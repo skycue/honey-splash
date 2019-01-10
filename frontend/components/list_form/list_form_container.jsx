9ab0c26d140a3144ed0f46069bbbe87852a9a8b9
Susanna <royalpolaris@gmail.com>
2019-01-10 15:08:41 -0500

    Add list errors and clear errors to props

--- frontend/components/list_form/list_form_container.jsx
+++ frontend/components/list_form/list_form_container.jsx
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import React from 'react';
-import { createList, updateList } from '../../actions/list_actions';
+import { createList, updateList, removeErrors } from '../../actions/list_actions';
 import ListForm from './list_form';
 import { closeModal } from '../../actions/modal_actions';
 
 const mapStateToProps = state => {
   return {
+    errors: state.errors.list,
     currentUserId: state.session.id,
     selectedListId: state.ui.selectedListId
   };
@@ -20,8 +21,9 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         return dispatch(updateList(currentUserId, list))
       }
     },
-    closeModal: () => dispatch(closeModal())
-  };
+    closeModal: () => dispatch(closeModal()),
+    clearErrors: () => dispatch(removeErrors());
+  }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListForm);
